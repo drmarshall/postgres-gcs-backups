8182ff02b88ca92706f3911ff9cceddc39b342db
drmarshall <6020152+drmarshall@users.noreply.github.com>
2023-10-10 11:30:16 -0700

    prettier

--- src/env.ts
+++ src/env.ts
@@ -7,21 +7,21 @@ export const env = envsafe({
   }),
   GCS_BUCKET: str(),
   BACKUP_DATABASE_URL: str({
-    desc: 'The connection string of the database to backup.'
+    desc: "The connection string of the database to backup.",
   }),
   BACKUP_CRON_SCHEDULE: str({
-    desc: 'The cron schedule to run the backup on.',
-    default: '0 5 * * *',
-    allowEmpty: true
+    desc: "The cron schedule to run the backup on.",
+    default: "0 5 * * *",
+    allowEmpty: true,
   }),
   BACKUP_PREFIX: str({
-    desc: 'Prefix for the backup file name.',
+    desc: "Prefix for the backup file name.",
     allowEmpty: true,
-    default: ''
+    default: "",
   }),
   RUN_ON_STARTUP: bool({
-    desc: 'Run a backup on startup of this application',
+    desc: "Run a backup on startup of this application",
     default: false,
     allowEmpty: true,
-  })
-})
+  }),
+});
